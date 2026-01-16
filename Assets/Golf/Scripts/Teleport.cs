05f3cdd0cf17a26b74f42c251e8c68e825b4ebb7
theduck5 <foxyfamily111@gmail.com>
2026-01-16 13:40:50 -0800

    5.6

---
+++ Assets/Golf/Scripts/Teleport.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    private GameObject target = GameObject.Find("Ball");
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (input)
+    }
+}
